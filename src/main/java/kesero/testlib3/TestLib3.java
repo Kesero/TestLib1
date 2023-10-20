878c78335d7d499d35e24432e3eb84d63ee77b3f
Kesero <127146733+Kesero@users.noreply.github.com>
2023-10-20 19:27:01 +0200

    Add files via upload

---
+++ src/main/java/kesero/testlib3/TestLib3.java
@@ -0,0 +1,11 @@
+package kesero.testlib3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestLib3 extends JavaPlugin {
+
+    public String GetTestText(){
+        return "__TEST3__";
+    }
+
+}
